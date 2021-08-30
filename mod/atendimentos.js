93ead3b67505345c4e1260258550581c3855205a
JacksonSanti <santi7728@gmail.com>
2021-08-30 09:11:44 -0300

    new commit

--- mod/atendimentos.js
+++ mod/atendimentos.js
@@ -1,6 +1,8 @@
+const { default: axios } = require('axios')
 const moment = require('moment')
 const { query } = require('../infraestrutura/conexao')
 const conexao = require('../infraestrutura/conexao')
+const { criarAtendimentos } = require('../infraestrutura/tabelas')
 const dataCriacao = require('../infraestrutura/tabelas')
 
 class Atendimento {
@@ -65,12 +67,15 @@ class Atendimento {
     buscaPorId(id, res){
         const sql =  `SELECT * FROM Atendimentos WHERE id=${id}`
 
-        conexao.query(sql, (erro, resultados) => {
+        conexao.query(sql, async (erro, resultados) => {
            const atendimento = resultados[0]
-           
+           const cpf = atendimento.cliente
+
             if(erro){
                 res.status(400).json(erro)
             } else {
+                const {data} = await axios.get(`http://localhost:8082/${cpf}`)
+                atendimento.cliente = data
                 res.status(200).json(atendimento)
             }
 
